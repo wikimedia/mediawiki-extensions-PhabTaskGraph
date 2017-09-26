6722ae731fb8bb39d99cc38614076cadff18c2fb
Cindy Cicalese <cindom@gmail.com>
2017-09-26 17:03:50 +0000

    First commit.
    Change-Id: Ibc2bfd3d81f07acf5c849620906e3452f1aacb12

---
+++ includes/SpecialPhabTaskGraph.php
@@ -0,0 +1,352 @@
+<?php
+
+require_once( $GLOBALS['wgPhabTaskGraphPhabLibPath'] . '/' .
+	'__phutil_library_init__.php');
+
+class SpecialPhabTaskGraph extends IncludableSpecialPage {
+
+	function __construct() {
+		parent::__construct( 'PhabTaskGraph' );
+	}
+
+	private $client = null;
+	private $nodes = [];
+	private $links = [];
+	private $projects = [];
+	private $people = [];
+
+	public function execute ( $parser ) {
+
+		$request = $this->getRequest();
+		$output = $this->getOutput();
+		$this->setHeaders();
+
+		$this->client = new ConduitClient( $GLOBALS['wgPhabTaskGraphPhabURL'] );
+		$api_token = $GLOBALS['wgPhabTaskGraphConduitAPIToken'];
+		$this->client->setConduitToken( $api_token );
+
+		$width = $request->getInt( 'width' );
+		if ( $width == 0 ) {
+			$width = 800;
+		}
+
+		$height = $request->getInt( 'height' );
+		if ( $height == 0 ) {
+			$height = 800;
+		}
+
+		$statusarray = $request->getArray( 'status' );
+		if ( $statusarray == [] ) {
+			$statusarray = [ 'open', 'stalled' ];
+		}
+
+		$tasks = $request->getText( 'tasks' );
+		if ( $tasks != '' ) {
+			$taskarray = array_unique( array_map( 'trim', explode( ',', $tasks ) ) );
+			foreach ( $taskarray as $i => $task ) {
+				if ( $task[0] === 'T' ) {
+					$taskarray[$i] = substr( $task, 1 );
+				}
+			};
+			$this->getTasks( $taskarray, $statusarray );
+		} else {
+			$taskarray = [];
+		}
+
+		$projects = $request->getText( 'projects' );
+		if ( $projects != '' ) {
+			$projectarray =
+				array_unique( array_map( 'trim', explode( ',', $projects ) ) );
+			$this->getProjects( $projectarray, $statusarray );
+			$mappedprojectarray = [];
+			foreach ( $this->projects as $project ) {
+				if ( in_array( $project['name'], $projectarray ) ) {
+					$mappedprojectarray[] = $project['phid'];
+				}
+			}
+			$projectarray = $mappedprojectarray;
+		} else {
+			$projectarray = [];
+		}
+
+		$nodearray = [];
+		$index = 0;
+		foreach ( $this->nodes as $n => $node ) {
+			$nodearray[$index] = $node;
+			$this->nodes[$n]['index'] = $index;
+			$index++;
+		}
+
+		$linkarray = [];
+		$index = 0;
+		foreach ( $this->links as $link ) {
+			$newlink = [];
+			$newlink['source'] = $this->nodes[$link['source']]['index'];
+			$newlink['target'] = $this->nodes[$link['target']]['index'];
+			$linkarray[$index] = $newlink;
+			$index++;
+		}
+
+		if ( $nodearray != [] ) {
+			$output->addModules( 'ext.PhabTaskGraph' );
+
+			$graphName = 'PhabTaskGraphDiv';
+			$data = [
+				'id' => $graphName,
+				'selected_tasks' => $taskarray,
+				'selected_projects' => $projectarray,
+				'nodes' => $nodearray,
+				'links' => $linkarray,
+				'projects' => $this->projects,
+				'people' => $this->people,
+				'url' => $GLOBALS['wgPhabTaskGraphPhabURL'],
+				'width' => $width,
+				'height' => $height
+			];
+			$output->addJsConfigVars( 'PhabTaskGraphConfig', $data );
+
+			$html = Html::element( 'div', array( 'id' => $graphName ) );
+
+			$output->addHTML( $html );
+		}
+
+		if ( !$this->including() ) {
+
+			$formDescriptor = [
+				'tasksfield' => [
+					'label-message' => 'phabtaskgraph-tasks-field-label',
+					'help-message' => 'phabtaskgraph-tasks-field-help',
+					'class' => 'HTMLTextField',
+					'default' => $tasks,
+					'name' => 'tasks'
+				],
+				'projectsfield' => [
+					'label-message' => 'phabtaskgraph-projects-field-label',
+					'help-message' => 'phabtaskgraph-projects-field-help',
+					'class' => 'HTMLTextField',
+					'default' => $projects,
+					'name' => 'projects'
+				],
+				'statusfield' => [
+					'label-message' => 'phabtaskgraph-status-field-label',
+					'type' => 'multiselect',
+					'options' => [
+						wfMessage('phabtaskgraph-status-open')->plain() => 'open',
+						wfMessage('phabtaskgraph-status-stalled')->plain() => 'stalled',
+						wfMessage('phabtaskgraph-status-resolved')->plain() => 'resolved',
+						wfMessage('phabtaskgraph-status-invalid')->plain() => 'invalid',
+						wfMessage('phabtaskgraph-status-declined')->plain() => 'declined',
+						wfMessage('phabtaskgraph-status-duplicate')->plain() => 'duplicate'
+					],
+					'default' => $statusarray,
+					'name' => 'status'
+				],
+				'widthfield' => [
+					'label-message' => 'phabtaskgraph-width-field-label',
+					'help-message' => 'phabtaskgraph-width-field-help',
+					'class' => 'HTMLTextField',
+					'default' => $width,
+					'name' => 'width'
+				],
+				'heightfield' => [
+					'label-message' => 'phabtaskgraph-height-field-label',
+					'help-message' => 'phabtaskgraph-height-field-help',
+					'class' => 'HTMLTextField',
+					'default' => $height,
+					'name' => 'height'
+				]
+			];
+
+			$htmlForm =
+				HTMLForm::factory( 'ooui', $formDescriptor, $this->getContext() );
+
+			$htmlForm->setMethod( 'get' );
+
+			$htmlForm->prepareForm()->displayForm( false );
+		}
+	}
+
+	private function getTasks( $tasks, $statusarray ) {
+		$tasklist = [];
+		foreach ( $tasks as $task ) {
+			$t = intval( $task );
+			if ( !isset( $this->nodes[$t] ) ) {
+				$tasklist[] = $t;
+			}
+		}
+		$params = [
+			'constraints' => [
+				'ids' => $tasklist,
+				'statuses' => $statusarray
+			],
+			'attachments' => [
+				'projects' => [
+					true
+				]
+			]
+		];
+		$result = $this->client->callMethodSynchronous( 'maniphest.search',
+			$params );
+		$data = $result['data'];
+		foreach ( $data as $datum ) {
+			$this->parseTask( $datum, $statusarray );
+		}
+	}
+
+	private function getProjects( $projects, $statusarray ) {
+		$params = [
+			'constraints' => [
+				'projects' => $projects,
+				'statuses' => $statusarray
+			],
+			'attachments' => [
+				'projects' => [
+					true
+				],
+				'columns' => [
+					true
+				]
+			]
+		];
+		$result = $this->client->callMethodSynchronous( 'maniphest.search',
+			$params );
+		$data = $result['data'];
+		foreach ( $data as $datum ) {
+			$this->parseTask( $datum, $statusarray );
+		}
+	}
+
+	private function parseTask( $data, $statusarray ) {
+		$taskID = $data['id'];
+		if ( isset( $this->nodes[$taskID] ) ) {
+			return;
+		}
+		$task = [];
+		$task['id'] = $taskID;
+		$task['phid'] = $data['phid'];
+		$task['taskid'] = 'T' . $data['id'];
+		$task['name'] = $data['fields']['name'];
+		$task['status'] = $data['fields']['status']['value'];
+		$task['color'] = $data['fields']['priority']['color'];
+		if ( $data['fields']['authorPHID'] ) {
+			$this->getPerson( $data['fields']['authorPHID'] );
+			$task['author'] = $data['fields']['authorPHID'];
+		} else {
+			$task['author'] = null;
+		}
+		if ( $data['fields']['ownerPHID'] ) {
+			$this->getPerson( $data['fields']['ownerPHID'] );
+			$task['owner'] = $data['fields']['ownerPHID'];
+		} else {
+			$task['owner'] = null;
+		}
+		$task['projects'] = [];
+		foreach ( $data['attachments']['projects']['projectPHIDs'] as $projphID ) {
+			$this->getProject( $projphID );
+			if ( array_key_exists( 'columns', $data['attachments'] ) &&
+				array_key_exists( $projphID,
+				$data['attachments']['columns']['boards'] ) ) {
+				$column = ' (' . $data['attachments']['columns']['boards'][$projphID]
+					['columns'][0]['name'] . ')';
+			} else {
+				$column = '';
+			}
+			$task['projects'][$projphID] = $this->projects[$projphID]['name'] . $column;
+		}
+		$this->nodes[$taskID] = $task;
+		$this->getSubtasks( $taskID, $statusarray );
+	}
+
+	private function getSubtasks( $parentTaskID, $statusarray ) {
+		$params = [
+			'constraints' => [
+				'parentIDs' => [
+					$parentTaskID
+				],
+				'statuses' => $statusarray
+			],
+			'attachments' => [
+				'projects' => [
+					true
+				],
+				'columns' => [
+					true
+				]
+			]
+		];
+		$data = $this->client->callMethodSynchronous( 'maniphest.search',
+			$params );
+		foreach ( $data['data'] as $task ) {
+			$taskID = $task['id'];
+			$this->parseTask( $task, $statusarray );
+			$link = [];
+			$link['source'] = $parentTaskID;
+			$link['target'] = $taskID;
+			$this->links[] = $link;
+		}
+	}
+
+	private function getProject( $projphID ) {
+		if ( isset( $this->projects[$projphID] ) ) {
+			return;
+		}
+		$params = [
+			'constraints' => [
+				'phids' => [
+					$projphID
+				],
+			]
+		];
+		$result = $this->client->callMethodSynchronous( 'project.search',
+			$params );
+		$data = $result['data'];
+		if ( count( $data ) > 0 ) {
+			$this->parseProject( $data[0] );
+		}
+	}
+
+	private function parseProject( $data ) {
+		$projphID = $data['phid'];
+		if ( isset( $this->projects[$projphID] ) ) {
+			return;
+		}
+		$project = [];
+		$project['id'] = $data['id'];
+		$project['phid'] = $projphID;
+		$project['name'] = $data['fields']['name'];
+		$project['color'] = $data['fields']['color']['key'];
+		$this->projects[$projphID] = $project;
+	}
+
+	private function getPerson( $personphID ) {
+		if ( isset( $this->people[$personphID] ) ) {
+			return;
+		}
+		$params = [
+			'constraints' => [
+				'phids' => [
+					$personphID
+				],
+			]
+		];
+		$result = $this->client->callMethodSynchronous( 'user.search',
+			$params );
+		$data = $result['data'];
+		if ( count( $data ) > 0 ) {
+			$this->parsePerson( $data[0] );
+		}
+	}
+
+	private function parsePerson( $data ) {
+		$personphID = $data['phid'];
+		if ( isset( $this->people[$personphID] ) ) {
+			return;
+		}
+		$person = [];
+		$person['id'] = $data['id'];
+		$person['phid'] = $personphID;
+		$person['username'] = $data['fields']['username'];
+		$person['realname'] = $data['fields']['realName'];
+		$this->people[$personphID] = $person;
+	}
+}
---
+++ resources/PhabTaskGraph.js
@@ -0,0 +1,322 @@
+﻿var phabtaskgraph_controller = ( function ( mw, $ ) {
+	'use strict';
+
+	return {
+		initialize: function () {
+			var graph = mw.config.get( 'PhabTaskGraphConfig' );
+			this.showGraph(graph.id, graph.selected_tasks, graph.selected_projects,
+				graph.nodes, graph.links, graph.projects, graph.people, graph.url,
+				graph.width, graph.height);
+		},
+		showGraph: function (id, selected_tasks, selected_projects, nodes, links,
+			projects, people, url, width, height) {
+			var radius = 8;
+			var padding = 5;
+
+			var selectedNode = -1;
+			var linkedByIndex = {};
+			links.forEach(function (d) {
+				linkedByIndex[d.source + ',' + d.target] = 1;
+			});
+
+			var force = d3
+				.layout
+				.force()
+				.charge(-400)
+				.linkDistance(120)
+				.size([width, height]);
+
+			var div = d3
+				.select('#' + id);
+
+			var tooltip = div
+				.append('div')
+				.attr('class', 'phabtaskgraph-tooltip')
+				.style('opacity', 0);
+
+			var zoom = d3
+				.behavior
+				.zoom()
+				.scaleExtent([0.5,5])
+				.on('zoom', zoomed);
+
+			var svg = div
+				.append('svg')
+				.attr('width', width)
+				.attr('height', height)
+				.attr('class', 'phabtaskgraph-graph')
+				.append('g')
+				.call(zoom);
+
+			svg
+				.on('click', function() {
+					if (d3.event.altKey) {
+						clearHighlight()
+					}
+				});
+
+			svg
+				.on('dblclick.zoom', null);
+
+			svg
+				.append('defs')
+				.selectAll('marker')
+				.data(['arrow'])
+				.enter()
+				.append('marker')
+				.attr('id', function (d) {
+					return d;
+				})
+				.attr('viewBox', '0 0 10 10')
+				.attr('refX', 20)
+				.attr('refY', 5)
+				.attr('markerWidth', 8)
+				.attr('markerHeight', 8)
+				.attr('orient', 'auto')
+				.append('path')
+				.attr('d', 'M 0 0 L 10 5 L 0 10 z')
+				.attr('class', 'phabtaskgraph-marker');
+
+			var rect = svg
+				.append('rect')
+				.attr('width', width)
+				.attr('height', height)
+				.style('fill', 'none')
+				.style('pointer-events', 'all');
+
+			var container = svg.append('g');
+
+			var link = container
+				.selectAll('.link')
+				.data(links)
+				.enter()
+				.append('line')
+				.attr('class', 'phabtaskgraph-link')
+				.style('marker-end', 'url(#arrow)');
+
+			var node = container
+				.selectAll('.node')
+				.data(nodes)
+				.enter()
+				.append('g')
+				.attr('class', 'phabtaskgraph-node');
+
+			node
+				.append('circle')
+				.attr('r', radius - 0.75)
+				.style('fill', function (d) {
+					return d.color;
+				})
+				.style('stroke', function (d) {
+					if (selected_tasks.includes(String(d.id))) {
+						return '#00f';
+					}
+					var key;
+					for (key in d.projects) {
+						if (selected_projects.includes(key)) {
+							return '#00f';
+						}
+					}
+					return '#fff';
+				});
+
+			node
+				.append('text')
+				.attr('dx', 10)
+				.attr('dy', '.35em')
+				.text(function (d) {
+					var name = d.name;
+					if (name.length > 30) {
+						name = name.substring(0,30) + '...';
+					}
+					return name;
+				})
+				.attr('class', 'phabtaskgraph-text');
+
+			node
+				.on('dblclick', function (d) {
+					window.open(url + '/' + d.taskid, '_blank');
+				});
+
+			node
+				.on('click', function (d) {
+					if (d3.event.altKey) {
+						highlightConnected(d);
+						d3.event.stopPropagation();
+					}
+				});
+
+			var stop_tooltip = false;
+			var stop_drag = false;
+
+			node
+				.on('mouseover', function (d) {
+					stop_tooltip = true;
+					force.stop();
+					tooltip
+						.transition()
+						.duration(200)
+						.style('opacity', .9);
+					tooltip
+						.html(format_tooltip(d))
+						.style("left", d3.event.offsetX + "px")
+						.style("top", (d3.event.offsetY - 28) + "px");
+				});
+
+			node
+				.on('mouseout', function (d) {
+					stop_tooltip = false;
+					if (!stop_drag) {
+						force.resume();
+					}
+					tooltip
+						.transition()
+						.duration(500)
+						.style('opacity', 0);
+				});
+
+			var drag = d3
+				.behavior
+				.drag();
+
+			drag
+				.on('dragstart', function (d) {
+					d3.event.sourceEvent.stopPropagation();
+					stop_drag = true;
+					force.stop();
+				});
+
+			drag
+				.on('drag', function (d) {
+					d.px += d3.event.dx;
+					d.py += d3.event.dy;
+					d.x += d3.event.dx;
+					d.y += d3.event.dy;
+					tick();
+				});
+
+			drag
+				.on('dragend', function (d) {
+					if (d3.event.sourceEvent.shiftKey) {
+						d.fixed = !d.fixed;
+					}
+					tick();
+					stop_drag = false;
+					if (!stop_tooltip) {
+						force.resume();
+					}
+				});
+
+			node
+				.call(drag);
+
+			force
+				.nodes(nodes)
+				.links(links)
+				.on('tick', tick)
+				.start();
+
+			function zoomed() {
+				container.attr('transform', 'translate(' + d3.event.translate +
+					')scale(' + d3.event.scale + ')');
+			}
+
+			function format_tooltip (d) {
+				var html = d.taskid + ' (' + d.status + ')<br />';
+				html += d.name + '<br />';
+				if (d.author) {
+					var author = people[d.author];
+					html +=
+						mw.message( 'phabtaskgraph-author-field-label' ).text() +
+ 						': ' + author.username;
+					if (author.realname) {
+						 html += ' (' + author.realname + ')';
+					}
+					html += '<br />';
+				}
+				if (d.owner) {
+					var owner = people[d.owner];
+					html +=
+						mw.message( 'phabtaskgraph-owner-field-label' ).text() +
+						': ' + owner.username;
+					if (owner.realname) {
+					 html += ' (' + owner.realname + ')';
+					}
+					html += '<br />';
+				}
+				Object.keys(d.projects).forEach( function (key) {
+					var color = projects[key].color;
+					if (color == 'disabled') {
+						color = 'grey';
+					}
+					html += '<span style="color:' + color + ';">';
+					html += d.projects[key];
+					html += '</span>';
+					html += '<br />';
+				});
+				return html;
+			}
+
+			function tick() {
+				link
+					.attr('x1', function (d) {
+						return d.source.x;
+					})
+					.attr('y1', function (d) {
+						return d.source.y;
+					})
+					.attr('x2', function (d) {
+						return d.target.x;
+					})
+					.attr('y2', function (d) {
+						return d.target.y;
+					});
+				node
+					.attr('transform', function (d) {
+						return 'translate(' + d.x + ',' + d.y + ')';
+					});
+			}
+
+			function neighboring(a, b) {
+				return a.index == b.index || linkedByIndex[a.index + ',' + b.index];
+			}
+
+			function highlightConnected(d) {
+				if (selectedNode != d.index) {
+					node.style('opacity', function (o) {
+						return neighboring(d, o) | neighboring(o, d) ? 1 : 0.1;
+					});
+					link.style('opacity', function (o) {
+						return d.index==o.source.index | d.index==o.target.index ? 1 : 0.1;
+					});
+					selectedNode = d.index;
+				} else {
+					node.style('opacity', 1);
+					link.style('opacity', 1);
+					selectedNode = -1;
+				}
+			}
+
+			function clearHighlight() {
+				svg
+					.selectAll('.phabtaskgraph-link')
+					.style('opacity', 1);
+				svg
+					.selectAll('.phabtaskgraph-node')
+					.style('opacity', 1);
+				selectedNode = -1;
+			}
+		}
+	};
+}( mediaWiki, jQuery ) );
+
+window.PhabTaskGraphController = phabtaskgraph_controller;
+
+( function ( mw, $ ) {
+	$( document )
+		.ready( function () {
+			if ( mw.config.exists( 'PhabTaskGraphConfig' ) ) {
+				window.PhabTaskGraphController.initialize();
+			}
+		} );
+}( mediaWiki, jQuery ) );
