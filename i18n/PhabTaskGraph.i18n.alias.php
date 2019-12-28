5ebc99f2855f341964ed8d8163f46c25572f68f1
Umherirrender <umherirrender_de.wp@web.de>
2019-12-28 13:31:45 +0100

    Split alias file for special page alias and magic word alias
    When languages are added to these files it better have it splitted.
    
    Change-Id: I07b06539e6d16fddace6a9a688eb08905ec34a20

--- i18n/PhabTaskGraph.i18n.alias.php
+++ i18n/PhabTaskGraph.i18n.alias.php
@@ -5,10 +5,3 @@ $specialPageAliases = [];
 $specialPageAliases['en'] = [
 	'PhabTaskGraph' => [ 'PhabTaskGraph', 'Phabricator Task Graph' ]
 ];
-
-$magicWords = [];
-
-$magicWords['en'] = [
-	'datemultilinegraph' => [ 0, 'datemultilinegraph' ],
-	'datebarchart' => [ 0, 'datebarchart' ]
-];
---
+++ i18n/PhabTaskGraph.i18n.magic.php
@@ -0,0 +1,8 @@
+<?php
+
+$magicWords = [];
+
+$magicWords['en'] = [
+	'datemultilinegraph' => [ 0, 'datemultilinegraph' ],
+	'datebarchart' => [ 0, 'datebarchart' ]
+];
